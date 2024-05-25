15cbdc859cacb10ee70c96a7d17fe278804bbf19
Samuel <jsamuel24577@gmail.com>
2024-05-26 01:55:24 +0530

    3 - patterns

--- patterns.py
+++ patterns.py
@@ -4,4 +4,19 @@ def firstpattern(n):
             print("*", end='')
         print()
 
+def secondpattern(n):
+    for i in range(n):
+        for j in range(i):
+            print("*", end='')
+        print()
+
+def thirdpattern(n):
+    for i in range(n):
+        for j in range(i):
+            print(j+1, end='')
+        print()
+
+
 firstpattern(5)
+secondpattern(5)
+thirdpattern(6)
